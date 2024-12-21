cfb52efa9078212a629e069fdb55f9a2b5c7ad96
17Arhaan <17arhaan@gmail.com>
2024-12-21 23:32:31 +0530

    pattern

--- DSA.cpp/patterns/pattern6.cpp
+++ DSA.cpp/patterns/pattern6.cpp
@@ -5,11 +5,9 @@ int main() {
     int n;
     cout << "Enter N : ";
     cin >> n;
-    int num=1;
-    for(int i = 1 ; i <= n ; i++){
-        for(int j = 1 ; j <= n ; j++){
-        cout << " " << num;
-        num++;
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < i+1; j++){
+        cout << " " << (i+1);
         }
         cout << endl;
     }
