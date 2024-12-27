defcc32e1fb720eaee78d4c281f6626a92225774
17Arhaan <17arhaan@gmail.com>
2024-12-28 01:54:44 +0530

    arrays

--- DSA.cpp/array.cpp
+++ DSA.cpp/array.cpp
@@ -4,16 +4,16 @@ int main(){
     int n;
     cout << "Enter Length : ";
     cin >> n;
-    int marks[n];
+    int arr[n];
     for (int i = 0 ; i < n ; i++ ){
         cout << "Enter Element [" << i+1 <<"] : ";
-        cin >> marks[i];
+        cin >> arr[i];
     }
     cout << endl;
     cout << "Array --->";
     cout << endl;
     for (int i = 0 ; i < n ; i++ ){
-        cout << marks[i] << " ";
+        cout << arr[i] << " ";
     }
     cout << endl;
     return 0;
--- DSA.cpp/reverseanarray.cpp
+++ DSA.cpp/reverseanarray.cpp
@@ -1,20 +1,51 @@
-#include<iostream>
+#include <iostream>
+#define max 99
 using namespace std;
-int main(){
+
+void reverseArray(int arr[max],int n){
+    int start = 0;
+    int end = n - 1;
+    while (start < end){
+        swap(arr[start],arr[end]);
+        start ++;
+        end --;
+    }
+}
+int main() {
     int n;
     cout << "Enter Length : ";
     cin >> n;
-    int marks[n];
-    for (int i = 0 ; i < n ; i++ ){
-        cout << "Enter Element [" << i+1 <<"] : ";
-        cin >> marks[i];
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter Element [" << i + 1 << "] : ";
+        cin >> arr[i];
+    }
+
+    cout << endl << "Array --->" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
     cout << endl;
-    cout << "Array --->";
-    cout << endl;
-    for (int i = 0 ; i < n ; i++ ){
-        cout << marks[i] << " ";
+
+    
+    // for (int i = 0, j = n - 1; i < j; i++, j--) {
+    //     swap(arr[i], arr[j]);
+    // }
+
+
+    // for (int i = 0; i < n / 2; i++) {
+    //     for (int j = n - i - 1; j > i; j--) {
+    //         swap(arr[i], arr[j]);
+    //         break; // Break after one swap to simulate nested loop behavior
+    //     }
+    // }
+    reverseArray(arr,n);
+    cout << "Reversed Array --->" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
     cout << endl;
+
     return 0;
 }
