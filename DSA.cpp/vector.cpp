958d55503001bbb8db1f317722130f27c066e479
17Arhaan <17arhaan@gmail.com>
2024-12-30 18:35:14 +0530

    vector

---
+++ DSA.cpp/vector.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printVector(const vector<int>& vec) {
+    cout << "Vector ---> ";
+    for (int i : vec) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter Length: ";
+    cin >> n;
+
+    vector<int> vec(n);
+    cout << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter Element [" << i + 1 << "]: ";
+        cin >> vec[i];
+    }
+
+    printVector(vec);
+    return 0;    
+}
