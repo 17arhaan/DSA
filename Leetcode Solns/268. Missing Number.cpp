b2eda55b13f6c103924f2dba5a10ff6c25427836
17Arhaan <17arhaan@gmail.com>
2025-01-31 02:02:39 +0530

    leetcode

---
+++ Leetcode Solns/268. Missing Number.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int xor1 = 0, xor2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            xor1 ^= nums[i];
+            xor2 ^= (i + 1);
+        }
+
+        return xor1 ^ xor2;
+    }
+};
